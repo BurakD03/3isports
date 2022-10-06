b160382787091a6f95b12bd3555364d0e1c9e224
BurakD03 <ufcboyka@hotmail.fr>
2022-10-06 23:00:42 +0200

    ajout WebpackEncore

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\DomCrawler\Crawler;
 
 class TestController extends AbstractController
 {
@@ -26,17 +27,54 @@ class TestController extends AbstractController
         $data = $crawler->filter('item > description')->each(function (Crawler $node, $i) {
             return $node->text();
         });
-        $data = $crawler->filter('item > enclosure')->each(function (Crawler $node, $i) {
+        $data = $crawler->filter('enclosure')->each(function (Crawler $node, $i) {
             // var_dump($node->attr('url'));
-            return $node->text();
+            // var_dump($node->text());
+            return $node->attr('url');
         });
+        $test = [];
+        $test = $crawler->filter('item')->each(function (Crawler $node, $i) {
+            $test['titre'] = $node->filter('title')->text();
+            // $test['description'] = $node->filter('description')->text('Default text content', false);
+            // var_dump($node->filterXPath('//item/description')->text('Default text content', false));
+            $test['description'] = $this->after('>', $node->filter('description')->text());
+            // $test['description'] = $this->forward('App\Controller\TestController::between', [
+            //     'string' => $node->filter('description')->text(),
+            //     ''
+            // ];
+            $test['auteur'] = $node->filterXPath('//dc:creator')->text();
+            $test['category'] = $node->filter('category')->text();
+            $test['datePublication'] = $node->filter('pubDate')->text();
+            $test['link'] = $node->filter('guid')->text();
+            return $test;
+        });
+        // var_dump($test);
         // $data = $crawler->filterXPath('//item/enclosure[contains(@url,"https")]')->evaluate('substring-after(@url, "")');
         // var_dump($images);
         // var_dump($title);
-        var_dump($data);
-        
+        // var_dump($data);
+
         return $this->render('test/index.html.twig', [
             'controller_name' => 'TestController',
+            'data'            => $data,
+            'test'            => $test
         ]);
+    }
+
+    public function before($string, $inthat)
+    {
+        return substr($inthat, 0, strpos($inthat, $string));
+    }
+
+    public function after($string, $inthat)
+    {
+        if (!is_bool(strpos($inthat, $string))) {
+            return substr($inthat, strpos($inthat, $string)+strlen($string));
+        }
+    }
+
+    public function between($string, $that, $inthat)
+    {
+        return $this->before($that, $this->after($string, $inthat));
     }
 }
